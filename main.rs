e2ea7f9bd28340d117e602d6c9b1a3ee5a0e59fc
Conor Dooley <mail@conchuod.ie>
2022-08-01 16:28:30 +0100

    ykush: add power {on,off} & reboot support
    Signed-off-by: Conor Dooley <mail@conchuod.ie>

--- main.rs
+++ main.rs
@@ -6,7 +6,24 @@
 use clap::Parser;
 use serde_yaml::Value;
 use std::{fs,process::Command};
-use std::fmt;
+use std::{fmt, thread, time};
+
+/// lab
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+	/// input yaml config file
+	#[clap(short, long, default_value = "config.yaml")]
+	config: String,
+
+	/// board to operate on
+	#[clap(short, long, default_value = "icicle")]
+	board: String,
+	
+	/// command (reboot, off, goodnight)
+	#[clap(short, long, default_value = "off")]
+	function: String,
+}
 
 #[derive(Debug)]
 struct ConfigParsingError {
@@ -31,6 +48,28 @@ impl std::error::Error for ConfigParsingError {
 	}
 }
 
+#[derive(Debug)]
+struct YkushcmdError {
+	details: String
+}
+
+impl YkushcmdError {
+	fn new(msg: &str) -> YkushcmdError {
+		YkushcmdError{details: msg.to_string()}
+	}
+}
+
+impl fmt::Display for YkushcmdError {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "ykushcmd failed: {}", self.details)
+	}
+}
+
+impl std::error::Error for YkushcmdError {
+	fn description(&self) -> &str {
+		&self.details
+	}
+}
 
 fn get_board_from_config(board: String, input_file: String, serial: &mut String, port: &mut String)
 -> Result<(), Box<dyn std::error::Error>>
@@ -62,36 +101,47 @@ fn get_board_from_config(board: String, input_file: String, serial: &mut String,
 	return Ok(());
 }
 
-/// lab
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-	/// input yaml config file
-	#[clap(short, long, default_value = "config.yaml")]
-	config: String,
+fn power(board: String, serial: String, port: String, direction: String)
+-> Result<(), Box<dyn std::error::Error>>
+{
+	let output = Command::new("sh")
+		.arg("-c")
+		.arg(
+			&format!("ykushcmd ykush -s {} -{} {}",
+				serial,
+				direction.chars().nth(0).unwrap(),
+				port)
+		)
+		.output()
+		.expect("failed to execute process");
 
-	/// board to operate on
-	#[clap(short, long, default_value = "icicle")]
-	board: String,
-	
-	/// shut down all boards
-	#[clap(long)]
-	goodnight: bool,
+	if !output.status.success() {
+		return Err(Box::new(YkushcmdError::new("failed to power direction")));
+	}
 
-	/// boot or reboot a specific board
-	#[clap(short, long)]
-	reboot: bool,
+	println!("{} attached to {}@{} powered {}.", board, serial, port, direction);
+	return Ok(())
 }
 
-fn main() -> Result<(),Box<dyn std::error::Error>> {
-	let args = Args::parse();
-	let input_file = args.config;
-	let board = args.board;
-	let mut serial: String = String::new();
-	let mut port: String = String::new();
-	
-	get_board_from_config(board.clone(), input_file, &mut serial, &mut port)?;
+fn power_down(board: String, serial: String, port: String)
+-> Result<(), Box<dyn std::error::Error>>
+{
+	power(board, serial, port, "down".to_string())?;
 
+	return Ok(())
+}
+
+fn power_up(board: String, serial: String, port: String)
+-> Result<(), Box<dyn std::error::Error>>
+{
+	power(board, serial, port, "up".to_string())?;
+
+	return Ok(())
+}
+
+fn reboot_board(board: String, serial: String, port: String)
+-> Result<(), Box<dyn std::error::Error>>
+{
 	let output = Command::new("sh")
 		.arg("-c")
 		.arg("ykushcmd ykush -l ")
@@ -103,20 +153,30 @@ fn main() -> Result<(),Box<dyn std::error::Error>> {
 		Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
 	};
 	if !stdout.clone().contains(&serial) {
-		return Err(Box::new(std::fmt::Error))
+		return Err(Box::new(YkushcmdError::new(&format!(
+			"board with serial {} not found", serial))))
 	}
 	
 	println!("{} attached to {}@{}", board, serial, port);
-
-	let output = Command::new("sh")
-		.arg("-c")
-		.arg(&format!("ykushcmd ykush -s {} -d {}", serial, port))
-		.output()
-		.expect("failed to execute process");
-
-	if output.status.success() {
-		println!("{} attached to {}@{} shut down.", board, serial, port);
-	}
+	power_down(board.clone(), serial.clone(), port.clone())?;
+	thread::sleep(time::Duration::from_millis(1000));
+	power_up(board.clone(), serial.clone(), port.clone())?;
 
 	return Ok(())
+}
+
+fn main() -> Result<(),Box<dyn std::error::Error>> {
+	let args = Args::parse();
+	let input_file = args.config;
+	let board = args.board;
+	let mut serial: String = String::new();
+	let mut port: String = String::new();
+	
+	get_board_from_config(board.clone(), input_file, &mut serial, &mut port)?;
+
+	match args.function.as_str() {
+		"off" => power_down(board, serial, port),
+		"on" | "reboot" => reboot_board(board, serial, port),
+		_ => Err(Box::new(YkushcmdError::new("Invalid function"))),
+	}
 }
